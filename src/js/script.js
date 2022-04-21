511e0c293f2d631112e372d38946c2718acba2b6
Shustovsky <a.shustovsky@gmail.com>
2022-04-21 12:08:44 +0400

    fix bags+work with js, carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-alert(12);
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 3000,
+        /* 
+                adaptiveHeight: true, */
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        /* responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }] */
+    });
+});
