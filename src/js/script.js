4fc3569aaee5dd62550450f12b2f060347fd318a
Shustovsky <a.shustovsky@gmail.com>
2022-05-03 12:48:58 +0400

    add mailer + fix bags

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,72 @@ document.querySelector('.next').addEventListener('click', function() {
         toggleSlide('.catalog-item__link');
         toggleSlide('.catalog-item__back');
 
+        //modal
 
+        $('[data-modal=consultation]').on('click', function() {
+            $('.overlay, #consultation').fadeIn('slow');
+        });
+        $('.modall__close').on('click', function() {
+            $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+        });
+
+        $('.button_mini').each(function(i) {
+            $(this).on('click', function() {
+                $('#order .modall__descr').text($('.catalog-item__subtitle').eq(i).text());
+                $('.overlay, #order').fadeIn('slow');
+            })
+        });
+
+
+        function validateForms(form) {
+            $(form).validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Пожалуйста введите своё имя",
+                        minlength: jQuery.validator.format("введите {0} символа!")
+                    },
+                    phone: "Пожалуйста введите свой номер телефона",
+                    email: {
+                        required: "Пожалуйста введите свой почтовый адрес",
+                        email: "неправельно введён адрес почты"
+                    }
+                }
+            });
+        }
+        validateForms('#consultation-form');
+        validateForms('#order form');
+        validateForms('#consultation form');
+
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+        $('form').submit(function(e) {
+            e.preventDefault();
+            /*  if (!$(this).valid()) {
+                 return;
+             } */
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+                $('form').trigger('reset');
+            });
+            return false;
+        });
 
     });
 })(jQuery);
